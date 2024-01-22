df8de7f3df50b393a809dcd216b1ff3a8aeb6d1c
David <david.galan.7e7@itb.cat>
2024-01-22 19:19:37 +0100

    Agregar archivos de proyecto.

---
+++ MethodLibraryHvM2/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MethodLibraryHvM2
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ TestProjectHvM/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestProjectHvM
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
