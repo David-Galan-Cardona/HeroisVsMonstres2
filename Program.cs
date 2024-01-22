13083f844688f1a6fb986724922215c4baa0b533
David <david.galan.7e7@itb.cat>
2024-01-22 19:24:14 +0100

    Added main Closes #1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+using MethodLibraryHvM2;
+namespace M03UF2PR1
+{
+    public class HeroisVsMonstre2
+    {
+
+
+        public static void Main()
+        {
+            const string Play = "Què vols fer? \n1. Iniciar una nova batalla\n0. Sortir";
+
+            int play, errors = 0;
+
+
+            Console.WriteLine(Play);
+            play = Convert.ToInt32(Console.ReadLine());
+            while ((play != 0 && play != 1) || errors == 3)
+            {
+                errors++;
+                Console.WriteLine(Play);
+                play = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
